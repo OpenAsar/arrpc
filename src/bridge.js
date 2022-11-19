48e9c75edef60543ba970ce149d66ffcc92c81a3
CanadaHonk <oj@oojmed.com>
2022-11-19 23:13:01 +0000

    bridge: add connected/disconnected notices

--- src/bridge.js
+++ src/bridge.js
@@ -1,3 +1,6 @@
+const rgb = (r, g, b, msg) => `\x1b[38;2;${r};${g};${b}m${msg}\x1b[0m`;
+const log = (...args) => console.log(`[${rgb(88, 101, 242, 'arRPC')} > ${rgb(87, 242, 135, 'bridge')}]`, ...args);
+
 import { WebSocketServer } from 'ws';
 
 // basic bridge to pass info onto webapp
@@ -6,3 +9,11 @@ export const send = msg => {
 };
 
 const wss = new WebSocketServer({ port: 1337 });
+
+wss.on('connection', socket => {
+  log('web connected');
+
+  socket.on('close', () => {
+    log('web disconnected');
+  })
+});
