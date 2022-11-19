ae37fdbd261e407fc1d0c9df3bb235a1cbd8ec9e
CanadaHonk <oj@oojmed.com>
2022-11-19 23:19:45 +0000

    bump version to 0.3.0

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const rgb = (r, g, b, msg) => `\x1b[38;2;${r};${g};${b}m${msg}\x1b[0m`;
 const log = (...args) => console.log(`[${rgb(88, 101, 242, 'arRPC')}]`, ...args);
 
-log('arRPC v0.2.0');
+log('arRPC v0.3.0');
 
 import Server from './server.js';
 const x = await new Server();
