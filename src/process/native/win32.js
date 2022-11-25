46ad54de626c592c42a297a88cea2cb1e80e4e19
CanadaHonk <oj@oojmed.com>
2022-11-25 19:43:28 +0000

    process/win32: parse pid as ints

--- src/process/native/win32.js
+++ src/process/native/win32.js
@@ -1,5 +1,9 @@
 import { exec } from 'child_process';
 
 export const getProcesses = () => new Promise(res => exec(`wmic process get ProcessID,ExecutablePath /format:csv`, (e, out) => {
-  res(out.toString().split('\r\n').slice(2).map(x => x.trim().split(',').slice(1).reverse()).filter(x => x[1]));
+  res(out.toString().split('\r\n').slice(2).map(x => {
+    const parsed = x.trim().split(',').slice(1).reverse();
+    parsed[0] = parseInt(parsed[0]) || parsed[0]; // pid to int
+    return parsed;
+  }).filter(x => x[1]));
 }));
