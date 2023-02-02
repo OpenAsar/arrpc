e3326025bb2d1b6dff628d0562a5f9b9f81c1b40
Ashton <akazshaw@gmail.com>
2023-02-01 21:09:16 -0600

    Fixed not showing in window

--- bin/my-bin.js
+++ bin/my-bin.js
@@ -2,9 +2,11 @@ var join = require('path').join
 var childProcess = require('child_process');
 var args = process.argv.slice(2);
 
- args.unshift(__dirname + '/../'); 
+args.unshift(__dirname + '/../'); 
 
-childProcess.exec('node src', (err, stdout) => {
-if (err) console.log(err);
-console.log(stdout);
-})
+var child = childProcess.exec('node src')
+
+child.stdout.on('data',
+    function (data) {
+        console.log(data);
+    });
