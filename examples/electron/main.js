fe18a3c5b23b760acdf5c66a0979f635b8246830
V <vendicated@riseup.net>
2023-06-29 19:53:26 +0200

    invite/guild-template listeners: Pass callback param

--- examples/electron/main.js
+++ examples/electron/main.js
@@ -4,6 +4,7 @@ import Server from './path/to/arrpc/server.js';
 
 const arrpc = await new Server();
 arrpc.on('activity', data => myWindow.webContents.send('rpc', data));
-arrpc.on('invite', code => {
+arrpc.on('invite', (code, callback) => {
   // your invite code handling here
+  // callback(true) // Reply back to tell the client whether the invite is valid or not 
 });
