bfcba7e3d6e6f7301a5699c4a1eb10c968e7b568
Justice Almanzar <superdash993@gmail.com>
2023-07-25 16:17:47 +0000

    Fix detection of most games running as admin (#40)

--- src/process/native/win32.js
+++ src/process/native/win32.js
@@ -1,9 +1,9 @@
 import { exec } from 'child_process';
 
-export const getProcesses = () => new Promise(res => exec(`wmic process get ProcessID,ExecutablePath /format:csv`, (e, out) => {
+export const getProcesses = () => new Promise(res => exec(`wmic process get ProcessID,ExecutablePath,Name /format:csv`, (e, out) => {
   res(out.toString().split('\r\n').slice(2).map(x => {
+    // [ProcessId, Name, ExecutablePath]
     const parsed = x.trim().split(',').slice(1).reverse();
-    parsed[0] = parseInt(parsed[0]) || parsed[0]; // pid to int
-    return parsed;
+    return [ parseInt(parsed[0]) || parsed[0], parsed[2] || parsed[1] ];
   }).filter(x => x[1]));
 }));
