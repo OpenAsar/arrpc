4bb10dfe103d88ae3741ca5481b9af5bf75ed72b
Justice Almanzar <superdash993@gmail.com>
2023-07-25 16:11:10 +0000

    code review

--- src/process/native/win32.js
+++ src/process/native/win32.js
@@ -2,9 +2,8 @@ import { exec } from 'child_process';
 
 export const getProcesses = () => new Promise(res => exec(`wmic process get ProcessID,ExecutablePath,Name /format:csv`, (e, out) => {
   res(out.toString().split('\r\n').slice(2).map(x => {
+    // [ProcessId, Name, ExecutablePath]
     const parsed = x.trim().split(',').slice(1).reverse();
-    parsed[0] = parseInt(parsed[0]) || parsed[0]; // pid to int
-    parsed[1] = parsed[2] || parsed[1]
-    return parsed.slice(0, 2);
+    return [ parseInt(parsed[0]) || parsed[0], parsed[2] || parsed[1] ];
   }).filter(x => x[1]));
 }));
