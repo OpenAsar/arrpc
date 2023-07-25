e703365d28dcd5ce03e3b1a8999f2de60b099a2b
Justice Almanzar <superdash993@gmail.com>
2023-07-25 15:45:03 +0000

    use process name as path fallback

--- src/process/native/win32.js
+++ src/process/native/win32.js
@@ -1,9 +1,10 @@
 import { exec } from 'child_process';
 
-export const getProcesses = () => new Promise(res => exec(`wmic process get ProcessID,ExecutablePath /format:csv`, (e, out) => {
+export const getProcesses = () => new Promise(res => exec(`wmic process get ProcessID,ExecutablePath,Name /format:csv`, (e, out) => {
   res(out.toString().split('\r\n').slice(2).map(x => {
     const parsed = x.trim().split(',').slice(1).reverse();
     parsed[0] = parseInt(parsed[0]) || parsed[0]; // pid to int
-    return parsed;
+    parsed[1] = parsed[2] || parsed[1]
+    return parsed.slice(0, 2);
   }).filter(x => x[1]));
 }));
