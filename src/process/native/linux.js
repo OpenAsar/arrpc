ec5ed0acb1995c1588c49cd7c86376029a5f0ced
Rini <142252300+rniii@users.noreply.github.com>
2023-08-14 15:21:37 -0300

    fix(linux): make process detection work regardless of `ps` impl (#46)

--- src/process/native/linux.js
+++ src/process/native/linux.js
@@ -1,17 +1,8 @@
-import { exec } from 'child_process';
-import { readlink } from 'fs/promises';
+import { readdir, readlink } from 'fs/promises';
 
-export const getProcesses = () => new Promise(res => exec(`ps a -o "%p;%c;%a"`, async (e, out) => {
-  res((await Promise.all(out.toString().split('\n').slice(1, -1).map(async x => {
-    const split = x.trim().split(';');
-    // if (split.length === 1) return;
-
-    const pid = parseInt(split[0].trim());
-    const cmd = split[1].trim();
-    const argv = split.slice(2).join(';').trim();
-
-    const path = await readlink(`/proc/${pid}/exe`).catch(e => {}); // read path from /proc/{pid}/exe symlink
-
-    return [ pid, path ];
-  }))).filter(x => x && x[1]));
-}));
+export const getProcesses = async () => {
+  const pids = (await readdir("/proc")).filter((f) => !isNaN(+f));
+  return (await Promise.all(pids.map((pid) =>
+    readlink(`/proc/${pid}/exe`).then((path) => [+pid, path], () => {})
+  ))).filter(x => x);
+}
