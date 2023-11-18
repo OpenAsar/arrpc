42e1206111d24fffd50fbe3e8fcb50321f9ce283
0xk1f0 <fteppan@protonmail.com>
2023-11-18 19:58:15 +0100

    fix(linux): game detection regardless of process wrapper

--- src/process/native/linux.js
+++ src/process/native/linux.js
@@ -1,8 +1,15 @@
-import { readdir, readlink } from 'fs/promises';
+import { readdir, readFile } from "fs/promises";
 
 export const getProcesses = async () => {
-  const pids = (await readdir("/proc")).filter((f) => !isNaN(+f));
-  return (await Promise.all(pids.map((pid) =>
-    readlink(`/proc/${pid}/exe`).then((path) => [+pid, path], () => {})
-  ))).filter(x => x);
-}
+  let pids = (await readdir("/proc")).filter((f) => !isNaN(+f));
+  return (
+    await Promise.all(
+      pids.map((pid) =>
+        readFile(`/proc/${pid}/cmdline`, "utf8").then(
+          (path) => [+pid, path.replace(/\0/g, "")],
+          () => {}
+        )
+      )
+    )
+  ).filter((x) => x);
+};
