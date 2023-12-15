2df5e636715a5c86588008221eb52d71503f70a9
0xk1f0 <dev@k1f0.dev>
2023-12-15 02:33:17 +0100

    fix(linux): make process scanning more efficient

--- src/process/native/linux.js
+++ src/process/native/linux.js
@@ -1,15 +1,21 @@
 import { readdir, readFile } from "fs/promises";
 
-export const getProcesses = async () => {
+export const getProcesses = async (currentProcs) => {
   let pids = (await readdir("/proc")).filter((f) => !isNaN(+f));
-  return (
+  let updatedProcs = currentProcs.filter((item) => pids.includes(item[0]));
+  let updatedPids = pids.filter(
+    (item) => !updatedProcs.map((item) => item[0]).includes(item)
+  );
+  let processes = (
     await Promise.all(
-      pids.map((pid) =>
+      updatedPids.map((pid) =>
         readFile(`/proc/${pid}/cmdline`, "utf8").then(
-          (path) => [+pid, path.replace(/\0/g, "")],
+          (path) => [pid, path.replace(/\0/g, "")],
           () => {}
         )
       )
     )
   ).filter((x) => x);
+  let adjustedList = [...processes, ...updatedProcs];
+  return adjustedList;
 };
